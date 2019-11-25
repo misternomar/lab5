9abff0f9ca278aae532c6274f760ef2d3d4c4c60
EC2 Default User <ec2-user@ip-172-31-23-22.us-west-2.compute.internal>
2019-11-25 08:05:25 +0000

    third commit

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const express = require("express");
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set("views", "./views/");
 app.use(express.static("public"));
 
 const request = require("request");
